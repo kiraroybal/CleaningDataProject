225aa5e3a6bfe360fa175db980120e44d19001be
kiraroybal <kiraroybal@gmail.com>
2018-11-26 13:31:06 -0800

    Update write.table

--- run_analysis.R
+++ run_analysis.R
@@ -98,6 +98,7 @@ colnames(all) <- gsub("^t", "Time", colnames(all))
 colnames(all) <- gsub("^f", "Freq", colnames(all))
 
 View(all)
+write.table(all, file = "allHAR.csv", row.names = FALSE)
 
 
 ##### Create a 2nd tidy data set (5) #####
@@ -120,3 +121,4 @@ newdata <- newdata[-c(2,3)]
 colnames(newdata) <- gsub("Avgsubject", "subject", colnames(newdata))
 
 View(newdata)
+write.table(newdata, file = "newdataHAR.csv", row.names = FALSE)
